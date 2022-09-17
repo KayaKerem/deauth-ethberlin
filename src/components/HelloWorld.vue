31883b7fe5836846b88e5a42fa67e1757c471252
Kerem Kaya <kerem@mgssoftware.net>
2022-09-18 00:29:48 +0300

    project initialized

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto">
+      <div>
+        <h3 class="text-center mb-0">DeAuth</h3>
+        <!-- <nav class="nav nav-masthead justify-content-center float-md-end"> -->
+        <!-- <a class="nav-link active" aria-current="page" href="#"
+            >My Accounts</a
+          > -->
+        <!-- <button type="button" class="btn btn-primary">My Accounts</button> -->
+        <!-- </nav> -->
+      </div>
+    </header>
+
+    <main class="px-3" style="padding-top: 100px">
+      <h1>Link your passwords to your wallet</h1>
+      <p class="lead">
+        Link the <b> passwords</b> of your social media accounts to your
+        <b> wallet. </b> Maximum <b>security </b>, minimum<b> memory.</b>
+      </p>
+      <p class="lead">
+        <button
+          v-if="!isConnected"
+          type="button"
+          class="btn btn-primary"
+          style="margin-top: 20px"
+          @click="connectWallet"
+        >
+          Connect Your Wallet
+        </button>
+      </p>
+    </main>
+
+    <footer class="mt-auto text-white-50">
+      <p>
+        Cover template for
+        <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by
+        <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.
+      </p>
+    </footer>
   </div>
 </template>
 
 <script>
+// import { ethers } from "ethers";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      isConnected: false,
+      jobName: "",
+      jobDesc: "",
+      currentAccount: "0x1883D25defF8C0F18CDd81fEe1067aC27327616d",
+      currentContract: null,
+      contractAddress: "0xedaE04603b092a06C927900002b47341F9f4C9c9",
+    };
+  },
+  methods: {
+    checkConnectedWalletExist: async function () {
+      try {
+        const { ethereum } = window;
+        if (!ethereum) {
+          alert("Make sure you have metamask!");
+
+          return false;
+        } else {
+          console.log("We have the ethereum object", ethereum);
+        }
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          this.currentAccount = account;
+          this.isConnected = false;
+          return true;
+        } else {
+          console.log("No authorized account found");
+
+          return false;
+        }
+      } catch (error) {
+        console.log(error);
+
+        return false;
+      }
+    },
+    connectWallet: async function () {
+      try {
+        const { ethereum } = window;
+        if (!ethereum) {
+          alert("Get MetaMask!");
+          return;
+        }
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        console.log("Connected", accounts[0]);
+        this.isConnected = true;
+        this.currentAccount = accounts[0];
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.checkConnectedWalletExist();
+    console.log(this.checkConnectedWalletExist());
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+/* @import url("https://fonts.googleapis.com/css2?family=Prompt&display=swap"); */
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
