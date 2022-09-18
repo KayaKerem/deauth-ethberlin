ca2d3fe5d5a21f764755b6144696870fec5be24e
Kerem Kaya <kerem@mgssoftware.net>
2022-09-18 03:05:06 +0300

    interface created. Wallet Connection and signature connection established

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Main from "./components/Main.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Main,
   },
 };
 </script>
